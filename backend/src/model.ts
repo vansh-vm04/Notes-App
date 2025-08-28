a32e80785016788c8d545f76da2d1da538faad03
Vansh <vanshmorwani1@gmail.com>
2025-08-29 00:17:27 +0530

    added otp schema

--- backend/src/model.ts
+++ backend/src/model.ts
@@ -2,8 +2,7 @@ import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema({
     name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String}
+    email:{type:String,required:true,unique:true}
 })
 
 const NoteSchema = new mongoose.Schema({
@@ -11,5 +10,14 @@ const NoteSchema = new mongoose.Schema({
     userId:{type:mongoose.Schema.Types.ObjectId, ref:'User', required:true}
 })
 
+const OtpSchema = new mongoose.Schema({
+    email:{type:String, required:true, unique:true},
+    otp:{type:String,required:true},
+    expiresAt: { type: Date, required: true },
+})
+
+OtpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
 export const UserModel = mongoose.model('User',UserSchema)
 export const NoteModel = mongoose.model('Note',NoteSchema)
+export const OtpModel = mongoose.model('Otp',OtpSchema)
