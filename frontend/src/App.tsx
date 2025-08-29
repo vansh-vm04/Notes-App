98c113a5e0d5054caae4cb010e6b737510e9f327
Vansh <vanshmorwani1@gmail.com>
2025-08-29 18:03:01 +0530

    some setup for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-
+import { Route, Routes } from "react-router";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
