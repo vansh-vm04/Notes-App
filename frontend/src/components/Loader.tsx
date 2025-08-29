f2a59f8462efa9ab15fa82001de393702630b78d
Vansh <vanshmorwani1@gmail.com>
2025-08-30 04:25:27 +0530

    loader component

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,11 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex bg-white fixed z-50 items-center justify-center h-screen w-screen">
+        <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loader
