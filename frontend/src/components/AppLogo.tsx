c8bb915548f0cae2b6b479ead2e4616f2e02ecc1
Vansh <vanshmorwani1@gmail.com>
2025-08-29 18:03:12 +0530

    added logo component

---
+++ frontend/src/components/AppLogo.tsx
@@ -0,0 +1,11 @@
+
+const AppLogo = () => {
+  return (
+    <div className="flex gap-2 bg-transparent h-fit w-fit items-center">
+        <img src="/icon.svg" alt="" />
+        <span className="font-bold text-2xl">HD</span>
+    </div>
+  )
+}
+
+export default AppLogo
