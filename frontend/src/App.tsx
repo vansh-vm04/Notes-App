c72f097a1805c179a4af1d062d871ff8ed862916
Vansh <vanshmorwani1@gmail.com>
2025-08-30 04:25:19 +0530

    routes and toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const location = useLocation();
+  const showNavbar = ['/'].includes(location.pathname);
+  
   return (
     <>
+    {showNavbar && <Navbar/>}
       <Routes>
         <Route path="/" element={<Dashboard/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp/>} />
       </Routes>
+      <ToastContainer autoClose={1500} position="top-right"/>
     </>
   );
 }
