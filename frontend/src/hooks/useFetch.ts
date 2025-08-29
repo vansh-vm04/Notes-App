78abf9ff1c0957e98cfff953a49c8cc97aaf0378
Vansh <vanshmorwani1@gmail.com>
2025-08-30 04:25:55 +0530

    hook for fetching notes

---
+++ frontend/src/hooks/useFetch.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export const useFetch = (url:string)=>{
+    const [loading, setloading] = useState(false)
+    const [data, setdata] = useState<unknown>({})
+    const [error, seterror] = useState<boolean>(false)
+
+    const fetchData = async (url:string)=>{
+        try {
+            setloading(true)
+            const token = localStorage.getItem('token')
+            const response = await axios.get(url,{
+                headers:{
+                    'Authorization':'Bearer '+token
+                }
+            })
+            setdata(response.data)
+        } catch {
+            seterror(true);
+        }finally{
+            setloading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData(url)
+    }, [url])
+    
+    return {
+        data,
+        loading,
+        error
+    }
+}
