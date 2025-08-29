67336792890fe8b1305598be9d393a943cebf256
Vansh <vanshmorwani1@gmail.com>
2025-08-30 04:25:35 +0530

    navbar added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,14 @@
+import { useAuth } from "../hooks/useAuth";
+
 const Navbar = () => {
+    const {logOut} = useAuth();
   return (
-    <nav className="fixed top-2 left-1/2 -translate-x-1/2 flex rounded-3xl justify-between max-md:w-sm w-[768px] items-center px-8 py-4 border-b bg-white shadow">
+    <nav className="fixed top-2 left-1/2 -translate-x-1/2 flex rounded-4xl justify-between max-md:w-full w-[768px] items-center px-8 py-4  bg-gray-100 shadow">
       <div className="flex items-center gap-4">
         <img src="/icon.svg" alt="" />
         <h1 className="text-lg font-semibold">Dashboard</h1>
       </div>
-      <button className="text-blue-600 hover:cursor-pointer hover:text-blue-800 hover:underline font-medium">
+      <button onClick={logOut} className="text-blue-600 underline hover:cursor-pointer hover:text-blue-800 font-medium">
         Sign Out
       </button>
     </nav>
