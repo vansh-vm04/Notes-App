b02331f437a46f4da2261cc0f5235839a9794d7d
Vansh <vanshmorwani1@gmail.com>
2025-08-30 16:15:40 +0530

    included google auth

--- backend/src/model.ts
+++ backend/src/model.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 
 const UserSchema = new mongoose.Schema({
     name:{type:String,required:true},
-    email:{type:String,required:true,unique:true}
+    email:{type:String,required:true,unique:true},
+    googleId:{type:String}
 })
 
 const NoteSchema = new mongoose.Schema({
--- backend/src/route.ts
+++ backend/src/route.ts
@@ -6,8 +6,18 @@ import { generateExpiry, generateOtp } from "./utils/otp";
 import { sendOtpMail } from "./utils/mailService";
 import { authMiddleware } from "./middleware";
 import { Request, Response } from "express";
+import { OAuth2Client } from "google-auth-library";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
+const oAuth2Client = new OAuth2Client();
+
+interface TicketPayload{
+    sub:string,
+    name:string,
+    email:string
+}
+
 
 router.post('/request-otp',async(req:Request,res:Response)=>{
     const {type,name,email} = req.body;
@@ -53,11 +63,34 @@ router.post('/verify-otp',async (req:Request,res:Response)=>{
         const user = await UserModel.findOne({email});
         if(!user) return res.status(404).json({message:"User not found"});
 
-        const token = jwt.sign({id:user._id},JWT_SECRET)
+        const token = jwt.sign({id:user._id},JWT_SECRET,{expiresIn:'1d'});
 
         res.status(200).json({token:token});
     } catch (error) {
         res.status(500).json({message:"Internal server error"})
+    }
+})
+
+router.post('/google-auth',async (req:Request,res:Response)=>{
+    const {credential} = req.body;
+    if(!credential) return res.status(401).json({message:"Unauthorized user"});
+    try {
+        const ticket = await oAuth2Client.verifyIdToken({
+            idToken:credential,
+            audience:CLIENT_ID
+        });
+        const payload = ticket.getPayload() as TicketPayload;
+        const {sub, email, name} = payload;
+        const user = await UserModel.findOneAndUpdate({email},{
+            name,
+            email,
+            googleId:sub
+        },{upsert:true,new:true});
+        const token = jwt.sign({id:user._id},JWT_SECRET,{expiresIn:'1d'});
+        res.status(200).json({token});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
     }
 })
 
