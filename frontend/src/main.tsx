66579ca598ef0c4d68c7272abf6deb91c3275fc1
Vansh <vanshmorwani1@gmail.com>
2025-08-30 16:15:50 +0530

    auth provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const env = import.meta.env;
 
 createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId={`${env.VITE_GOOGLE_CLIENT_ID}`}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </GoogleOAuthProvider>
 )
